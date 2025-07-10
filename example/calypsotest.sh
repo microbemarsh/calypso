6d6f9595740f5cd704403f5e132159f065536aff
Austin Marshall <75810734+microbemarsh@users.noreply.github.com>
2025-07-10 14:34:17 -0500

    Update README.md

--- example/calypsotest.sh
+++ example/calypsotest.sh
@@ -4,20 +4,20 @@
 #SBATCH --account=commons
 #SBATCH --partition=debug
 #SBATCH --time=00:30:00
-#SBATCH --mail-user=am503@rice.edu
+#SBATCH --mail-user=email@email.com
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --threads-per-core=1
 #SBATCH --mem=128GB
 #SBATCH --export=ALL
 
-source /home/am503/Documents/calypso_pyenv/bin/activate
+source /path/to/calypso_pyenv/bin/activate
 
-DB_DIR="/home/am503/Documents/calypso/utils/gtdb220_16S_emudb"
+DB_DIR="/path/to/calypso/utils/gtdb220_16S_emudb"
 
-python /home/am503/Documents/calypso/calypso.py \
-  --query_dir /home/am503/Documents/calypso/example/data \
+python /path/to/calypso/calypso.py \
+  --query_dir /path/to/calypso/example/data \
   --ref $DB_DIR/species_taxid.fasta \
   --tax_file $DB_DIR/taxonomy.tsv \
-  --outdir /home/am503/Documents/calypso/example/example_results \
+  --outdir /path/to/calypso/example/example_results \
   --qc 0.9 --pid 99 --raiseto 2.7 --threads 8
